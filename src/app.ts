f697cb6ff62859cf82066ea8fe26a99f032832aa
Carol-Rocha <caroldsr1@gmail.com>
2023-04-09 14:31:34 -0300

    feat: routes post, update and delete created

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,22 @@
-import express, { Application, Request, Response } from "express"
+import express, { Application } from "express"
 import { startDataBase } from "./database"
-import { createMovies } from "./logic"
+import {
+  createMovies,
+  deleteMovie,
+  listMovieById,
+  listMovies,
+  updateMovie,
+} from "./logic"
+import { checkMovieIdExistsMiddleware } from "./middlewares"
 
 const app: Application = express()
 app.use(express.json())
 
 app.post("/movies", createMovies)
+app.get("/movies", listMovies)
+app.get("/movies/:id", checkMovieIdExistsMiddleware, listMovieById)
+app.patch("/movies/:id", checkMovieIdExistsMiddleware, updateMovie)
+app.delete("/movies/:id", checkMovieIdExistsMiddleware, deleteMovie)
 
 const PORT: number = 3000
 const runningMsg = `Server running on http://localhost:${PORT}`
--- src/logic.ts
+++ src/logic.ts
@@ -2,25 +2,117 @@ import { Request, Response } from "express"
 import { client } from "./database"
 import { QueryConfig, QueryResult } from "pg"
 import { IMovie, TMovieRequest } from "./interfaces"
+import format from "pg-format"
 
 const createMovies = async (req: Request, res: Response): Promise<Response> => {
   const movieData: TMovieRequest = req.body
 
-  const queryString: string = `
+  const queryString: string = format(
+    `
    INSERT INTO 
      movies 
-     ("name", "category", "duration", "price")
+     (%I)
    VALUES
-     ($1, $2, $3, $4)
+     (%L)
     RETURNING *;
-   `
-  const queryConfig: QueryConfig = {
-    text: queryString,
-    values: Object.values(movieData),
-  }
-  const queryResult: QueryResult<IMovie> = await client.query(queryConfig)
-  console.log(queryConfig)
+   `,
+    Object.keys(movieData),
+    Object.values(movieData)
+  )
+
+  const queryResult: QueryResult<IMovie> = await client.query(queryString)
+
   return res.status(201).json(queryResult.rows[0])
 }
 
-export { createMovies }
+const listMovies = async (req: Request, res: Response): Promise<Response> => {
+  const category: any = req.query.category
+
+  let queryString: string = ""
+  let queryResult: QueryResult
+
+  if (category) {
+    queryString = `
+    SELECT 
+          * 
+    FROM 
+         movies
+    WHERE
+         category = $1;
+   `
+    const queryConfig: QueryConfig = {
+      text: queryString,
+      values: [category],
+    }
+
+    queryResult = await client.query(queryConfig)
+  } else {
+    queryString = `
+    SELECT 
+           * 
+    FROM 
+          movies;
+    `
+    queryResult = await client.query(queryString)
+  }
+
+  return res.status(200).json(queryResult.rows)
+}
+
+const listMovieById = async (
+  req: Request,
+  res: Response
+): Promise<Response> => {
+  const movie = res.locals.movie
+
+  return res.status(200).json(movie)
+}
+
+const updateMovie = async (req: Request, res: Response): Promise<Response> => {
+  const movieData: Partial<TMovieRequest> = req.body
+
+  const id: number = parseInt(req.params.id)
+
+  const queryString: string = format(
+    `
+    UPDATE
+        movies
+        SET(%I) = ROW(%L)
+    WHERE
+        id = $1
+    RETURNING *;
+   `,
+    Object.keys(movieData),
+    Object.values(movieData)
+  )
+
+  const queryConfig: QueryConfig = {
+    text: queryString,
+    values: [id],
+  }
+
+  const queryResult: QueryResult<IMovie> = await client.query(queryConfig)
+
+  return res.status(200).json(queryResult.rows[0])
+}
+
+const deleteMovie = async (req: Request, res: Response): Promise<Response> => {
+  const id: number = parseInt(req.params.id)
+
+  const queryString: string = `
+  DELETE 
+      from
+  movies 
+      WHERE 
+  id = $1;
+`
+  const queryConfig: QueryConfig = {
+    text: queryString,
+    values: [id],
+  }
+
+  await client.query(queryConfig)
+  return res.status(204).send()
+}
+
+export { createMovies, listMovies, listMovieById, updateMovie, deleteMovie }
